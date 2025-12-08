d4dc88957a9edf2ccc429fec3bfe1f76d2f5791b
temcha <itemchur@spin.ai>
2025-12-08 16:44:38 +0200

    added mocked tickets data, added basic navigation links

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,11 +1,23 @@
+import { initialTickets } from "@/data";
+
 type TicketPageProps = {
   params: Promise<{ ticketId: string }>;
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
   const { ticketId } = await params;
+  const ticket = initialTickets.find((t) => t.id === ticketId);
 
-  return <div className="text-lg">TicketPage: {ticketId}</div>;
+  if (!ticket) {
+    return <div>Ticket not found</div>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  );
 };
 
 export default TicketPage;
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import { initialTickets } from "@/data";
+
 const TicketsPage = () => {
-  return <div className="text-lg">TicketsPage</div>;
+  return (
+    <div>
+      {initialTickets.map((ticket) => (
+        <div key={ticket.id}>
+          <h2 className="text-lg">{ticket.title}</h2>
+
+          <Link href={`/tickets/${ticket.id}`} className="text-sm underline">
+            View
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TicketsPage;
