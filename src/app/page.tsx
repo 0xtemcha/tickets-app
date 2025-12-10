8244646b3b210e0f4e974a3558acd2ec7ede9ce2
temcha <itemchur@spin.ai>
2025-12-10 18:14:39 +0200

    added heading component in home and tickets pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Link from "next/link";
+import { Heading } from "@/components/heading";
 import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-y-8 flex-1">
-      <div>
-        <h2 className="text-3xl font-bold tracking-tight">Home</h2>
-        <p className="text-sm text-muted-foreground">
-          Your home place to start
-        </p>
-      </div>
+      <Heading title="Home" description="Your home place to start" />
 
       <div className="flex flex-col items-center flex-1">
         <Link href={ticketsPath()} className="text-sm underline">
